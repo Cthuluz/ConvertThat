1fd1778f94cd590c511bf0017902f13a09e8909c
Isabel Nelson <inels19@wgu.edu>
2024-01-31 15:16:38 -0700

    Add sections for input and output. Change conversion section into two with type of unit and unit

--- ConvertThat/Content-ViewModel.swift
+++ ConvertThat/Content-ViewModel.swift
@@ -10,10 +10,27 @@ import SwiftUI
 
 extension ContentView {
     @MainActor class ViewModel: ObservableObject {
+        // Item object and list of Item objects
         @Published var item = Item(name: "Water", density: 1)
         @Published var items = [Item]()
-        @Published var convDir = "Mass → Volume"
-        @Published var convDirs = ["Mass → Volume", "Volume → Mass"]
+        
+        // Selected type and list of measurement types
+        @Published var convFirst = "Weight"
+        @Published var convSecond = "Weight"
+        @Published var convDirs = ["Weight", "Mass", "Volume"]
+        
+        // Selected conversion units
+        @Published var unitFirst = "mg"
+        @Published var unitSecond = "oz"
+        
+        // Conversion unit arrays
+        @Published var masses = ["mg", "g", "kg"]
+        @Published var weights = ["oz", "lb"]
+        @Published var volumes = ["ml", "L", "fl oz", "tsp", "Tbsp", "cup", "pint", "quart", "gallon"]
+        
+        // Input and output variables
+        @Published var amountInput = 0.00
+        @Published var resultValue = 0.00
         
         init() {
             self.items.append(Item(name: "Water", density: 1))
--- ConvertThat/ContentView.swift
+++ ConvertThat/ContentView.swift
@@ -9,11 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var vm = ViewModel()
+    @FocusState var amountIsFocused: Bool
     
     var body: some View {
         NavigationStack {
             Form {
-                Section("Choose type:") {
+                Section("Choose type") {
                     Picker("Item:", selection: $vm.item) {
                         ForEach(vm.items, id: \.self) {
                             Text($0.name)
@@ -21,16 +22,53 @@ struct ContentView: View {
                     }
                 }
                 
-                Section("Direction of conversion:") {
-                    Picker("Direction:", selection: $vm.convDir) {
+                Section("Units from") {
+                    Picker("From:", selection: $vm.convFirst) {
                         ForEach(vm.convDirs, id: \.self) {
                             Text($0)
                         }
                     }
-                    .pickerStyle(.segmented)
+                    
+                    Picker("Unit:", selection: $vm.unitFirst) {
+                        ForEach(vm.masses + vm.weights, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section("Units to") {
+                    Picker("To:", selection: $vm.convSecond) {
+                        ForEach(vm.convDirs, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    
+                    Picker("Unit:", selection: $vm.unitSecond) {
+                        ForEach(vm.volumes, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section("Input") {
+                    TextField("Username", value: $vm.amountInput, format: .number.precision(.significantDigits(3)))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                }
+                
+                Section("Result") {
+                    Text(String(format: "%.2f", vm.resultValue))
+                        .bold()
                 }
             }
             .navigationTitle("ConvertThat")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
